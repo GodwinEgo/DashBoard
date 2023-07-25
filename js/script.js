eb26d6cb7a3db07e003b6f0f35bbe27f5b080a2e
GodwinEgo <godwinehile04@gmail.com>
2023-07-25 22:00:52 +0100

    data

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//sample data for the chart
+const chartData = {
+  labels: ["January", "February", "March", "April", "May", "June", "July"],
+  datasets: [
+    {
+      label: "Data Set 1",
+      data: [10, 15, 25, 30, 20, 35, 40],
+      backgroundColor: "rgba(255,99,132,0.2)",
+      borderColor: "rgba(255,99,132,1)",
+      borderWidth: 1,
+    },
+  ],
+};
